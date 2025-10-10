be6102f5599f811cb55a10c7a0a65bb1298a7d13
Brobgonal Second <yulianaacostats@gmail.com>
2025-10-09 19:37:27 -0500

    forgot to commit these

---
+++ src/music/profiles/medleys.asm
@@ -0,0 +1,25 @@
+// Playlist created by: Brobgonal Second/Brob2nd.
+// Features medley themes and arrangements.
+// Always call add_music_profile first to create the profile
+add_music_profile(medleys, "Medleys")
+
+// Then call add_to_music_profile for every track you want toggled on for this profile
+add_to_music_profile(medleys, POKEFLOATS)
+add_to_music_profile(medleys, PORKY_MEDLEY)
+add_to_music_profile(medleys, NBA_JAM)
+add_to_music_profile(medleys, FZERO_MEDLEY)
+add_to_music_profile(medleys, STARFOX_MEDLEY)
+add_to_music_profile(medleys, GB_MEDLEY)
+add_to_music_profile(medleys, FE_MEDLEY)
+add_to_music_profile(medleys, WIDE_UNDERWATER)
+add_to_music_profile(medleys, LINKS_AWAKENING_MEDLEY)
+add_to_music_profile(medleys, SMB2_MEDLEY)
+add_to_music_profile(medleys, OEDO_EDO)
+add_to_music_profile(medleys, BRAWL_OOT)
+add_to_music_profile(medleys, DOOM1)
+add_to_music_profile(medleys, BATTLE_GOLD_SILVER)
+add_to_music_profile(medleys, GANONMEDLEY)
+add_to_music_profile(medleys, TALENTSTUDIO)
+add_to_music_profile(medleys, DK_MEDLEY)
+add_to_music_profile(medleys, FZEROX_MEDLEY)
+add_to_music_profile(medleys, FE6_MEDLEY)
