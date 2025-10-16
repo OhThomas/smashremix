2b1762ec1ad616526ad4a03e1d7b2ed8e79a318c
Brobgonal Second <yulianaacostats@gmail.com>
2025-10-15 22:49:59 -0500

    oopsie

--- src/CharacterSelect.asm
+++ src/CharacterSelect.asm
@@ -5157,6 +5157,9 @@ scope CharacterSelect {
         lli     t2, Character.id.CBPEPPY
         beql    a1, t2, _draw_icon          // If CBPEPPY, then draw CBPEPPY stock icon
         addiu   a1, at, VARIANT_ICON_OFFSET.CBPEPPY // a1 = CBPEPPY footer struct
+                lli     t2, Character.id.SANDBAG
+        beq     a1, t2, _sandbag            // If SANDBAG, then draw SANDBAG stock icon
+        sll     t2, t2, 0x0002              // t2 = SANDBAG * 4 (offset in struct table)
         lli     t2, Character.id.BOSS
         bne     a1, t2, _gdk                // If not Master Hand, then skip... otherwise, draw Master Hand stock icon
         addiu   a1, at, VARIANT_ICON_OFFSET.MASTER_HAND // a1 = Master Hand footer struct
@@ -5177,7 +5180,7 @@ scope CharacterSelect {
         lw      t2, 0x0028(t1)              // t2 = main character file address pointer
         lw      t2, 0x0000(t2)              // t2 = main character file address
         beqz    t2, _gdk
-        lw      t1, 0x0060(t1)              // t1 = offset to attribute dataAdd commentMore actions
+        lw      t1, 0x0060(t1)              // t1 = offset to attribute data
         addu    t1, t2, t1                  // t1 = attribute data address
         lw      t1, 0x0340(t1)              // t1 = pointer to stock icon footer address
         b       _draw_icon
