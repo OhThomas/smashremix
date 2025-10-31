69f4af65012c219f3afa9e2ec60d52ddce05286d
OhThomas <thomasdonnrader@yahoo.com>
2025-10-31 15:41:59 -0500

    Ban on release
    Ban on release
    - Changed banning based on button release instead of press
    - Fixed stage resetting instead of banning
    
    Thank you Brob2nd
    
    Co-Authored-By: Brob2nd <158646138+Brob2nd@users.noreply.github.com>

--- src/Stages.asm
+++ src/Stages.asm
@@ -2246,20 +2246,20 @@ scope Stages {
         bnez    v0, _ban_reset              // if pressed, reset bans
         nop
 
-        // checking for C-Up press to ban non legal stages
+        // checking for C-Up release to ban non legal stages
         li      a0, Joypad.CU               // a0 - C-Up button mask 
-        li      a2, Joypad.PRESSED          // a2 - type
-        jal     Joypad.check_buttons_all_   // v0 = C-Up pressed
+        li      a2, Joypad.RELEASED         // a2 - type
+        jal     Joypad.check_buttons_all_   // v0 = C-Up released
         nop
-        bnez    v0, _non_legal_ban          // if pressed, ban non legal stages
+        bnez    v0, _non_legal_ban          // if released, ban non legal stages
         nop
         
-        // checking for C-Down press to ban stage
+        // checking for C-Down release to ban stage
         li      a0, Joypad.CD               // a0 - C-Down button mask 
-        li      a2, Joypad.PRESSED          // a2 - type
-        jal     Joypad.check_buttons_all_   // v0 = C-Down pressed
+        li      a2, Joypad.RELEASED         // a2 - type
+        jal     Joypad.check_buttons_all_   // v0 = C-Down released
         nop
-        beqz    v0, _end                    // if not pressed, skip
+        beqz    v0, _end                    // if not released, skip
         nop
 
         // checking if on RANDOM stage, don't ban if so
@@ -2423,8 +2423,8 @@ scope Stages {
         li      t0, bans_table              // t0 = pointer to bans_table
         li      t1, -1                      // t1 = -1 (banned)
         sh      t1, 0x0006(t0)              // banning stage 7,8
-        sw      r0, 0x0008(t0)              // setting bans 9-12 as 0 to reset
-        sw      r0, 0x000C(t0)              // setting bans 13-16 as 0 to reset
+        sw      t1, 0x0008(t0)              // banning stages 9-12
+        sw      t1, 0x000C(t0)              // banning stages 13-16
         sb      t1, 0x0010(t0)              // banning stage 17
 
         // t8 = Y, setting up for first row to ban
